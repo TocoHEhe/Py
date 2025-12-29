8b34d32a74173727fc0aedf9e8000c3d480686c9
TocoHEhe <thutvi79@gmail.com>
2025-12-29 13:42:34 +0700

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+STORAGE_DIR=/opt/render/project/.render/chrome
+if [ ! -d "$STORAGE_DIR" ]; then
+  mkdir -p $STORAGE_DIR
+  cd $STORAGE_DIR
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x google-chrome-stable_current_amd64.deb .
+fi
